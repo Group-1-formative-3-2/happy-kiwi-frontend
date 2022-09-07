95a4be4cf623440289fe22717f24561055a49465
Vincez9211 <109566455+Vincez9211@users.noreply.github.com>
2022-09-07 14:52:15 +1200

    Vue Router completed

--- happy-kiwi/src/main.js
+++ happy-kiwi/src/main.js
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Home from './components/HomePage.vue'
+import ExplorePage from './components/ExplorePage.vue'
+import LoginPage from './components/LoginPage.vue'
+import SlideShowPage from './components/SlideShowPage.vue'
+import SignUpPage from './components/SignUpPage.vue'
+
+const router = createRouter({
+   history: createWebHistory(), 
+   routes: [
+      {path: '/', name: 'Home', component: Home},
+      {path: '/explore', name: 'Explore', component: ExplorePage},
+      {path: '/login', name: 'Login', component: LoginPage}, 
+      {path: '/slideshow', name: 'SlideShow', component: SlideShowPage}, 
+      {path: '/signup', name: 'SignUp', component: SignUpPage},
+
+   ]
+})
+
+createApp(App)
+.use(router)
+.mount('#app')
