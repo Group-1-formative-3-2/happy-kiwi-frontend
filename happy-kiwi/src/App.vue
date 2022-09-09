ef4f543de91d0dca0660f60337f46ff34393be41
Vincez9211 <109566455+Vincez9211@users.noreply.github.com>
2022-09-09 14:24:26 +1200

    Slideshow added

--- happy-kiwi/src/App.vue
+++ happy-kiwi/src/App.vue
@@ -1,17 +1,17 @@
+
 <template>
-  <div class="header">
+  <div>
+    <div class="header">
    <a href="/"><img src="./assets/Happy Kiwi Logo-01.png" alt=""></a>
    <div class="router_links">
      <router-link to="/"  style="color: #000;">Home</router-link>
      <router-link to="/explore"  style="color: #000;">Explore</router-link>
      <router-link to="/login"  style="color: #000;">Login</router-link>
      <router-link to="/slideshow"  style="color: #000;">View</router-link>
-     <router-view></router-view>
-     
-     
-   </div>
-   
+     </div>
  </div>
+ <router-view></router-view>
+  </div>
 </template>
 
 <script>
---
+++ happy-kiwi/src/components/SlideShowPage.vue
@@ -0,0 +1,72 @@
+<template>
+   <div>
+    <transition-group name="fade" tag="div">
+      <div v-for="i in [currentIndex]" :key="i">
+        <img class="slideShowImg" :src="currentImg" />
+      </div>
+    </transition-group>
+    <!-- <a class="prev" @click="prev" href="#">&#10094;</a>
+    <a class="next" @click="next" href="#">&#10095;</a> -->
+</div>
+ </template>
+ <script>
+   export default {
+     name: "Slider",
+     data() {
+       return {
+         images: [
+            "https://i.ibb.co/YpssfNw/cameron-witney-Y3-Ohm-Jb-Z-yw-unsplash.jpg",
+            "https://i.ibb.co/qk4F49C/phil-botha-t-INX9-IIet-IQ-unsplash.jpg",
+            "https://i.ibb.co/K2xVgSp/ray-aucott-x-B0e8b-DV4ww-unsplash.jpg",
+            "https://i.ibb.co/PFFGgq1/luca-calderone-j-Vo0v-um-Ak4-unsplash.jpg"
+         ],
+         timer: null,
+         currentIndex: 0
+       };
+     },
+   
+     mounted: function() {
+       this.startSlide();
+     },
+   
+     methods: {
+       startSlide: function() {
+         this.timer = setInterval(this.next, 4000);
+       },
+   
+       next: function() {
+         this.currentIndex += 1;
+       },
+       prev: function() {
+         this.currentIndex -= 1;
+       }
+     },
+   
+     computed: {
+       currentImg: function() {
+         return this.images[Math.abs(this.currentIndex) % this.images.length];
+       }
+     }
+   };
+   </script>
+
+<style>
+   .fade-enter-active,
+   .fade-leave-active {
+   transition: opacity 1s ease;
+   }
+
+   .fade-enter-from,
+   .fade-leave-to {
+   opacity: 0;
+   }
+
+   .slideShowImg{
+      height: 100vh;
+      width: 100vw;
+      object-fit: cover;
+      z-index: 100;
+      position:fixed;
+   }
+
+</style>
