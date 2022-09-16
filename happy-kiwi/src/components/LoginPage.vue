ec2ae46a4fe542e55c69e639c6dd695e7bba802e
KayZ <270159822@yoobeestudent.ac.nz>
2022-09-16 21:14:02 +1200

    Update LoginPage.vue

--- happy-kiwi/src/components/LoginPage.vue
+++ happy-kiwi/src/components/LoginPage.vue
@@ -1,96 +1,88 @@
 <template>
-  <div class='createPostContainer'>
-    <div id='bg'>
-        <img class='back' src='../assets/Login_Page.jpg' alt=''>
+  <div class="createPostContainer">
+    <div id="bg">
+      <img class="back" src="../assets/Login_Page.jpg" alt="" />
     </div>
-    <div class='boxWrapper'>
-   
-      <form 
-      class='post' 
-      v-if="isLogin" 
-      @submit.prevent = "login"
-      >
-        <div><h2 class='head centerContainer'>Login</h2></div>
-     
-          <label for='inputEmail1' class='title'>E-mail:</label>
-          <input
-          class='createBox'
-            pattern='[A-Za-z0-9._+-]+@[A-Za-z0-9 -]+\.[a-z]{2,}'
-            v-model='formValue.email'
-            id='email'
-            required
-            type='email'
-            placeholder='Enter a valid email address'
-          />
-          <br> 
+    <div class="boxWrapper">
+      <form class="post" v-if="isLogin" @submit.prevent="login">
+        <div><h2 class="head centerContainer">Login</h2></div>
 
-          <label for='InputPassword1' class='title'>Password:</label>
-          <input
-            class='createBox'
-            pattern='[a-zA-Z0-9]{8,}'
-            v-model='formValue.password'
-            id='password'
-            required
-            type='password'
-            placeholder='At least 8 characters'
-          />
-          <br>
-          
-          <div class='spaceContainer'>
-            <router-link to='/signup' class="signUpLink">Sign Up</router-link>
-            <span class="passwordLink">Forgot Password</span>
-          </div>
-          <br>
+        <label for="inputEmail1" class="title">E-mail:</label>
+        <input
+          class="createBox"
+          pattern="[A-Za-z0-9._+-]+@[A-Za-z0-9 -]+\.[a-z]{2,}"
+          v-model="formValue.email"
+          id="email"
+          required
+          type="email"
+          placeholder="Enter a valid email address"
+        />
+        <br />
 
-        
+        <label for="InputPassword1" class="title">Password:</label>
+        <input
+          class="createBox"
+          pattern="[a-zA-Z0-9]{8,}"
+          v-model="formValue.password"
+          id="password"
+          required
+          type="password"
+          placeholder="At least 8 characters"
+        />
+        <br />
 
-          <div class='centerContainer'>
-            <!-- <button type='submit' class='postBtn'>Go</button> -->
-          <input type='submit' class='postBtn' value="Go"/>
+        <div class="spaceContainer">
+          <router-link to="/signup" class="signUpLink">Sign Up</router-link>
+          <span class="passwordLink">Forgot Password</span>
         </div>
-        <br>
-        <div class="centerContainer">{{errorMessage}}</div>
+        <br />
+
+        <div class="centerContainer">
+          <!-- <button type='submit' class='postBtn'>Go</button> -->
+          <input type="submit" class="postBtn" value="Go" />
+        </div>
+        <br />
+        <div class="centerContainer">{{ errorMessage }}</div>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-import SignUpPage from './SignUpPage.vue';
-const apiUsers ='https://api-users-login.netlify.app/.netlify/functions/api';
+import SignUpPage from "./SignUpPage.vue";
+const apiUsers = "https://api-users-login.netlify.app/.netlify/functions/api";
 export default {
   data() {
     return {
-    users: [],
-    loggedUser: '',
-    isLogin: true,    
-    formValue: {
-      email: '',
-      password: '',
-    },
-    errorMessage:''
-  }
-},
+      users: [],
+      loggedUser: "",
+      isLogin: true,
+      formValue: {
+        email: "",
+        password: "",
+      },
+      errorMessage: "",
+    };
+  },
 
   components: {
     SignUpPage,
   },
 
-  methods: { 
-
+  methods: {
     login() {
-      console.log("go")
+      console.log("go");
       this.users.forEach((users) => {
         if (
           users.email == this.formValue.email &&
           users.password == this.formValue.password
         ) {
-          this.loggedUser = users.firstname + ' ' + users.lastname;
+          this.loggedUser = users.firstname + " " + users.lastname;
           this.isLogin = true;
           localStorage.userId = users._id;
           localStorage.loggedUser = this.loggedUser;
         } else {
-          this.errorMessage = "Email and password must be match!"
+          this.errorMessage = "Email and password must be match!";
         }
       });
     },
@@ -99,7 +91,7 @@ export default {
   mounted() {
     // check if user is logged-in, do not show login form
     if (localStorage.loggedUser) {
-      this.user = false;s
+      this.user = false;
     }
 
     // get all users
@@ -115,6 +107,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
